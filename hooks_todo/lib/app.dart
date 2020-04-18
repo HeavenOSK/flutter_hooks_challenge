bc0c8134c5802a2dab0afa7aa82873b1e4e1d58f
heavenOSK <ryunosukeheaven.ba@gmail.com>
2020-04-18 11:42:36 +0900

    reaname

--- hooks_todo/lib/app.dart
+++ hooks_todo/lib/app.dart
@@ -8,7 +8,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Home(),
+      home: TasksPage(),
     );
   }
 }
--- hooks_todo/lib/pages/tasks/tasks.dart
+++ hooks_todo/lib/pages/tasks/tasks.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:hooks_todo/models/models.dart';
 import 'package:hooks_todo/models/tasks/tasks_manager.dart';
 import 'package:provider/provider.dart';
 
@@ -8,8 +7,8 @@ import 'hook.dart';
 import 'input_bottom_sheet.dart';
 
 /// A page which manages [Tasks].
-class Home extends StatelessWidget {
-  const Home({Key key}) : super(key: key);
+class TasksPage extends StatelessWidget {
+  const TasksPage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
